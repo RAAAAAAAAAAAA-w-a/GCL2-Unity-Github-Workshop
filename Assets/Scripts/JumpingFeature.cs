d03cfbf4e883d234f74227645470251f41c5cea6
Cleo Cheong <cleolcheong13.25@ichat.sp.edu.sg>
2026-07-20 21:27:34 +0800

    Cleo
    Edited jumping feature (not yet done)

--- Assets/Scripts/JumpingFeature.cs
+++ Assets/Scripts/JumpingFeature.cs
@@ -9,72 +9,49 @@ public class JumpingFeature : MonoBehaviour
     public Sprite newSprite;
 
     private SpriteRenderer spriteRenderer;
+    private Collider2D platformCollider;
+
     private bool hasBeenUsed = false;
 
-    void Start()
+    private void Start()
     {
+        platformCollider = GetComponent<Collider2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player") && !hasBeenUsed)
-        {
-            Rigidbody2D playerRb = other.GetComponent<Rigidbody2D>();
+        if (!other.CompareTag("Player") || hasBeenUsed)
+            return;
 
-            if (playerRb != null)
+        Rigidbody2D playerRb = other.GetComponent<Rigidbody2D>();
+
+        if (playerRb != null)
+        {
+            //gives the player an upward jump boost
+            playerRb.linearVelocity = new Vector2(
+                playerRb.linearVelocity.x,
+                jumpForce
+            );
+
+            //changes the platform's sprite
+            if (newSprite != null)
             {
-                // gives the player an upward jump boost
-                playerRb.linearVelocity = new Vector2(
-                    playerRb.linearVelocity.x,
-                    jumpForce
-                );
-
-                // replace the current sprite
-                if (newSprite != null)
-                {
-                    spriteRenderer.sprite = newSprite;
-                }
-
-                hasBeenUsed = true;
+                spriteRenderer.sprite = newSprite;
             }
+
+            //to disable the platform collider
+            platformCollider.enabled = false;
+
+            hasBeenUsed = true;
         }
-    }
-
-}
-
-    /*public class Jump
-    {
-
-        protected Collider2D collider;
-
-        protected override void Start()
-        {
-            base.Start();
-            collider = GetComponent<Collider2D>();
-        }
-
-        protected override void OnCollisionEnter2D(Collision2D collision)
-        {
-            if (collision.gameObject.layer != LayerMask.NameToLayer("Ground"))
-                return;
-            collider.isTrigger = true;
-            rb.linearVelocity = Vector2.zero;
-        }
-
-        protected virtual void OnTriggerExit2D(Collider2D other)
-        {
-            if (other.gameObject.layer != LayerMask.NameToLayer("Ground"))
-                return;
-            collider.isTrigger = false;
-        }
-
     }
 
     // Update is called once per frame
     void Update()
-        {
-        
-        }
+    {
+
+    }
+
+
 }
-   */
