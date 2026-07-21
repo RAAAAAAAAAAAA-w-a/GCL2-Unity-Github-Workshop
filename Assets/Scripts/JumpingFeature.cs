5170617ded10cdf3a59fac3b6c40b66d1e921d66
Cleo Cheong <cleolcheong13.25@ichat.sp.edu.sg>
2026-07-21 23:55:24 +0800

    Cleo
    completed jumping feature

--- Assets/Scripts/JumpingFeature.cs
+++ Assets/Scripts/JumpingFeature.cs
@@ -1,12 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
 using System.Threading;
 using UnityEngine;
-using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 
 public class JumpingFeature : MonoBehaviour
 {
-    public float jumpForce = 15f;
-    public Sprite newSprite;
+    [SerializeField] private float jumpForce = 15f;
+    [SerializeField] private Sprite newSprite;
+
+    // Drag the upper platform's BoxCollider2D into this field in Unity
+    [SerializeField] private Collider2D upperPlatform;
+
+    [SerializeField] private float ignoreCollisionTime = 1f;
 
     private SpriteRenderer spriteRenderer;
     private Collider2D platformCollider;
@@ -28,22 +34,48 @@ public class JumpingFeature : MonoBehaviour
 
         if (playerRb != null)
         {
-            //gives the player an upward jump boost
+            // Give the player an upward jump boost
             playerRb.linearVelocity = new Vector2(
                 playerRb.linearVelocity.x,
                 jumpForce
             );
 
-            //changes the platform's sprite
+            // Change the jumping machine's sprite
             if (newSprite != null)
             {
                 spriteRenderer.sprite = newSprite;
             }
 
-            //to disable the platform collider
-            platformCollider.enabled = false;
+            // Get the player's collider
+            Collider2D playerCollider = other.GetComponent<Collider2D>();
+
+            // Let the player pass through the upper platform temporarily
+            if (playerCollider != null && upperPlatform != null)
+            {
+                Physics2D.IgnoreCollision(
+                    playerCollider,
+                    upperPlatform,
+                    true
+                );
+
+                StartCoroutine(ReEnableCollision(playerCollider));
+            }
 
             hasBeenUsed = true;
+        }
+    }
+
+    private IEnumerator ReEnableCollision(Collider2D playerCollider)
+    {
+        yield return new WaitForSeconds(ignoreCollisionTime);
+
+        if (playerCollider != null && upperPlatform != null)
+        {
+            Physics2D.IgnoreCollision(
+                playerCollider,
+                upperPlatform,
+                false
+            );
         }
     }
 
